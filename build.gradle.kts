fc7e371c60ec3a164eb3b4e3f5f176b9c36957aa
Claus Polanka <claus.polanka@erstegroup.com>
2023-04-14 18:48:02 +0200

    Convert tabs to spaces

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "com.gildedrose"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
-	useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
-	}
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-	kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "17"
 }
